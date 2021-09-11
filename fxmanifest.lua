bb5ac53865e2b0c78b1fd43966d4c50fed5aeded
Hagen <hagen@paw.bot>
2021-09-11 15:23:36 +0100

    chore: add repository information

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,10 @@
 fx_version 'cerulean'
 game 'gta5'
 
+repository 'https://github.com/TFNRP/axonbody3'
+version '0.0.1'
+author 'Reece Stokes <hagen@hyena.gay>'
+
 client_script 'client.lua'
 server_script 'server.lua'
 ui_page 'index.html'
